e836136eab607118382a45265aa4a4dce3bc9f1c
mattrossman <rossman@gatech.edu>
2022-02-17 03:38:50 -0500

    Return unmount function from mount

--- src/excalidraw-app/hubs.tsx
+++ src/excalidraw-app/hubs.tsx
@@ -3,4 +3,8 @@ import ExcalidrawApp from ".";
 
 export function mount(domElement: HTMLElement, link?: string) {
   ReactDOM.render(<ExcalidrawApp link={link} />, domElement);
+  const unmount = () => {
+    ReactDOM.unmountComponentAtNode(domElement);
+  };
+  return unmount;
 }
